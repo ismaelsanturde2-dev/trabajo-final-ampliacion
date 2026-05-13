8c79264533a80895f982d360af93e59f7c4792a4
ismaelsanturde2-dev <ismael.santurde2@gmail.com>
2026-05-13 10:04:06 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "<h1>esto esta funcionando</h1>";
+/**
+ * Plugin Name: Tarea Larga
+ * Description: Este es mi plugin para la tarea.
+ * Version: 1.0
+ * Author: Tu Nombre
+ */
+
+// Aquí debajo va el código que hace funcionar al plugin
+echo "<h1>¡Hola! Este es mi archivo PHP de TareaLarga</h1>";
 ?>
